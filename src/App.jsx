4193f66d711981d8b5e6674957fff2d5973e0ebb
DelmMV <opi588@yandex.ru>
2024-04-14 16:24:37 +0300

    fix router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
-import { HashRouter as Router } from 'react-router-dom'; // Изменение здесь
+import {
+  HashRouter, Route, RouterProvider, Routes,
+} from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { FilterProvider } from './contexts/FilterContext';
 import { SearchProvider } from './contexts/SearchContext';
 import { SelectorFilterProvider } from './contexts/SelectorFilterContext';
 import { router } from './router/router';
+import { MapComponent } from './pages/map.jsx';
+import { Main } from './pages/main.jsx';
+import { Layout } from './components/Layout.jsx';
+import { PageNotFound } from './pages/page-not-found.jsx';
+import { Login } from './pages/login.jsx';
 
 const queryClient = new QueryClient();
 
@@ -14,7 +21,16 @@ function App() {
       <SelectorFilterProvider>
         <FilterProvider>
           <QueryClientProvider client={queryClient}>
-            <Router router={router} />
+            <HashRouter>
+              <Routes>
+                <Route path="/" element={<Layout />}>
+                  <Route index element={<Main />} />
+                  <Route path="map" element={<MapComponent />} />
+                </Route>
+                <Route path="*" element={<PageNotFound />} />
+                <Route path="login" element={<Login />} />
+              </Routes>
+            </HashRouter>
           </QueryClientProvider>
         </FilterProvider>
       </SelectorFilterProvider>
