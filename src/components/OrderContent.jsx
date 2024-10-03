18457bc489cc7a3266a47265f725852330a096b5
DelmMV <opi588@yandex.ru>
2024-10-03 17:03:21 +0300

    add category

--- src/components/OrderContent.jsx
+++ src/components/OrderContent.jsx
@@ -1,74 +1,92 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import {
-  Box, Checkbox, Image, Stack, Text,
+  Box, Checkbox, Image, Text, Stack, Divider,
 } from '@chakra-ui/react';
 import plugImg from '../assets/image/logo-t85.jpeg';
 
 function OrderContent({ getOrder, selectedItems, handleCheckboxChange }) {
+  const groupedProducts = useMemo(() => {
+    const groups = {};
+    getOrder.forEach((product) => {
+      if (!groups[product.CatalogName]) {
+        groups[product.CatalogName] = [];
+      }
+      groups[product.CatalogName].push(product);
+    });
+    return groups;
+  }, [getOrder]);
+
   return (
-    <>
-      {getOrder.map((product) => (
-        <React.Fragment key={product.RowId}>
-          <Box display="flex" justifyContent="space-between" alignItems="center" flexDir="row" height="70px">
-            <Box display="flex" alignItems="center">
-              <Checkbox
-                isChecked={selectedItems[product.RowId] || false}
-                onChange={() => handleCheckboxChange(product.RowId)}
-                paddingRight="5px"
-              />
-              <Image
-                marginRight="6px"
-                borderRadius="10px"
-                borderWidth="0"
-                boxShadow="md"
-                width="45px"
-                height="45px"
-                src={product.PictureId ? `https://app.tseh85.com/Service/api/image?PictureId=${product.PictureId}` : plugImg}
-                alt={product.ProductName}
-              />
-              <Box display="flex" flexDir="column">
-                <Text fontSize="md" fontWeight="bold" width="220px">{product.ProductName}</Text>
-                <Text fontSize="sm" fontWeight="bold" color="grey">{product.CatalogName}</Text>
-              </Box>
-            </Box>
-            <Box display="flex" alignItems="center" flexDir="column">
-              <Text fontSize="sm" fontWeight="bold" color={product.Quantity > 1 ? 'red' : 'inherit'}>
-                {product.Quantity}
-                {' '}
-                шт.
-              </Text>
-              <Text fontSize="12px" color="grey" fontWeight="bold">
-                {product.Amount}
-                {' '}
-                Р
-              </Text>
-            </Box>
-          </Box>
-          {product.Products && product.Products.length > 0 && (
-          <Stack pl="40px" spacing={1}>
-            {product.Products.map((nestedProduct) => (
-              <Box key={nestedProduct.RowId} display="flex" justifyContent="space-between" alignItems="center" flexDir="row" height="25px">
-                <Box display="flex" alignItems="center">
-                  <Image
-                    marginRight="6px"
-                    borderRadius="10px"
-                    width="25px"
-                    height="25px"
-                    src={nestedProduct.PictureId ? `https://app.tseh85.com/Service/api/image?PictureId=${nestedProduct.PictureId}` : plugImg}
-                    alt={nestedProduct.ProductName}
-                  />
-                  <Box display="flex" flexDir="row">
-                    <Text marginRight="5px" fontSize="13" fontStyle="italic" fontWeight="extrabold" color="#8b00ff">{nestedProduct.ProductName}</Text>
-                    <Text fontSize="13" color="#8b00ff">{nestedProduct.Quantity}</Text>
+    <Stack spacing={1}>
+      {Object.entries(groupedProducts).map(([catalogName, products]) => (
+        <Box key={catalogName}>
+          <Text fontSize="md" fontWeight="bold" color="gray.500">{catalogName}</Text>
+          <Stack spacing={2}>
+            {products.map((product) => (
+              <React.Fragment key={product.RowId}>
+                <Box display="flex" justifyContent="space-between" alignItems="center" flexDir="row" height="70px">
+                  <Box display="flex" alignItems="center">
+                    <Checkbox
+                      isChecked={selectedItems[product.RowId] || false}
+                      onChange={() => handleCheckboxChange(product.RowId)}
+                      paddingRight="5px"
+                    />
+                    <Image
+                      marginRight="6px"
+                      borderRadius="10px"
+                      borderWidth="0"
+                      boxShadow="md"
+                      width="45px"
+                      height="45px"
+                      src={product.PictureId ? `https://app.tseh85.com/Service/api/image?PictureId=${product.PictureId}` : plugImg}
+                      alt={product.ProductName}
+                    />
+                    <Box display="flex" flexDir="column">
+                      <Text fontSize="md" fontWeight="bold" width="220px">{product.ProductName}</Text>
+                    </Box>
+                  </Box>
+                  <Box display="flex" alignItems="center" flexDir="column">
+                    <Text fontSize="sm" fontWeight="bold" color={product.Quantity > 1 ? 'red' : 'inherit'}>
+                      {product.Quantity}
+                      {' '}
+                      шт.
+                    </Text>
+                    <Text fontSize="12px" color="grey" fontWeight="bold">
+                      {product.Amount}
+                      {' '}
+                      Р
+                    </Text>
                   </Box>
                 </Box>
-              </Box>
+                {product.Products && product.Products.length > 0 && (
+                  <Stack pl="40px" spacing={1}>
+                    {product.Products.map((nestedProduct) => (
+                      <Box key={nestedProduct.RowId} display="flex" justifyContent="space-between" alignItems="center" flexDir="row" height="25px">
+                        <Box display="flex" alignItems="center">
+                          <Image
+                            marginRight="6px"
+                            borderRadius="10px"
+                            width="25px"
+                            height="25px"
+                            src={nestedProduct.PictureId ? `https://app.tseh85.com/Service/api/image?PictureId=${nestedProduct.PictureId}` : plugImg}
+                            alt={nestedProduct.ProductName}
+                          />
+                          <Box display="flex" flexDir="row">
+                            <Text marginRight="5px" fontSize="13" fontStyle="italic" fontWeight="extrabold" color="#8b00ff">{nestedProduct.ProductName}</Text>
+                            <Text fontSize="13" color="#8b00ff">{nestedProduct.Quantity}</Text>
+                          </Box>
+                        </Box>
+                      </Box>
+                    ))}
+                  </Stack>
+                )}
+              </React.Fragment>
             ))}
           </Stack>
-          )}
-        </React.Fragment>
+          <Divider mt={2} mb={2} />
+        </Box>
       ))}
-    </>
+    </Stack>
   );
 }
 
